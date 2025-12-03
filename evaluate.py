fix: base model causal mask

evaluate.py
@@ -4,6 +4,7 @@ import argparse
 import pickle
 import math
 from model import GPTConfig, GPT
+from model_baseline import BaselineGPT
 from model_rope import GPTWithRoPE
 
 def evaluate(model, data_loader, device):
@@ -51,7 +52,7 @@ if __name__ == '__main__':
         model = GPTWithRoPE(model_config)
         print("Using GPTWithRoPE model.")
     else:
-        model = GPT(model_config)
+        model = BaselineGPT(model_config)
         print("Using BaselineGPT model.")
 
     # Load the model state
@@ -63,7 +64,7 @@ if __name__ == '__main__':
     batch_size = ckpt_config.get('batch_size', 64)  # Default to 64 if not specified
 
     # Load validation data
-    val_data = np.memmap(f'data/{args.dataset}/val.bin', dtype=np.uint16, mode='r')
+    val_data = np.memmap(f'data/{args.dataset}/test.bin', dtype=np.uint16, mode='r')
     val_data = torch.from_numpy(val_data.astype(np.int64))
 
     # Create sequences of block_size
@@ -90,4 +91,4 @@ if __name__ == '__main__':
     # Evaluate
     val_loss = evaluate(model, val_loader, device)
     bpc = val_loss / math.log(2)
-    print(f"Validation Loss: {val_loss:.4f}, Bits per character (bpc): {bpc:.4f}")
+    print(f"Test Loss: {val_loss:.4f}, Bits per character (bpc): {bpc:.4f}")

model_baseline.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import math
+from model import CausalSelfAttention
 
 # Define GPTBlock with MultiheadAttention
 class GPTBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
-        self.attn = nn.MultiheadAttention(config.n_embd, config.n_head, dropout=config.dropout)
+        # self.attn = nn.MultiheadAttention(config.n_embd, config.n_head, dropout=config.dropout)
+        self.attn = CausalSelfAttention(config)
         self.drop = nn.Dropout(config.dropout)
         self.ln_2 = nn.LayerNorm(config.n_embd)
 
@@ -25,7 +27,7 @@ class GPTBlock(nn.Module):
         x_ln = self.ln_1(x)
 
         # Self-attention uses x_ln as query, key, and value
-        attn_output, _ = self.attn(x_ln, x_ln, x_ln)
+        attn_output = self.attn(x_ln)
         x = x + self.drop(attn_output)
 
         # Feedforward block with residual connection